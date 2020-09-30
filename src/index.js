bac946c4fae826fe68c2930297247874d7851b4f
apfpo <47034754+apfpo@users.noreply.github.com>
2020-09-30 13:44:24 +0300

    fix: update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-
 exports.min = function min (array) {
-  return 0;
+  return array !== undefined && array.length !== 0 ? array.sort(
+  	function(a, b) { return a - b; } )[0] : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return  array !== undefined && array.length !== 0 ? array.sort(
+  	function(a, b) { return a - b; }).reverse()[0] : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return  array !== undefined && array.length !== 0 ? array.reduce((prev, curr, ind)=>{
+    return prev = prev + curr; })/array.length : 0; 
 }
